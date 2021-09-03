9a7b8d8171e0f8e51886342c88660248ef08cffd
Ethan Andersen <AndersenEthanG@gmail.com>
2021-09-03 14:33:35 -0600

    Budget layout established and basics connected

--- Budgeter/Budgeter/View Controllers/Budget/ExpenseDetailViewController.swift
+++ Budgeter/Budgeter/View Controllers/Budget/ExpenseDetailViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class ExpenseDetailViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var expenseNameLabel: UITextField!
+    @IBOutlet weak var expenseAmountLabel: UITextField!
+    @IBOutlet weak var expenseDatePicker: UIDatePicker!
+    
+    
+    // MARK: - Properties
+//    var expense: Expense?
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    } // End of View did load
 
-        // Do any additional setup after loading the view.
-    }
-
-}
+    
+    // MARK: - Actions
+    @IBAction func saveBtn(_ sender: Any) {
+        
+    } // End of Save Button
+    
+} // End of Class
