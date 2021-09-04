faf40c44ea49845b0725ad302a50c52d86e0227d
Ethan Andersen <AndersenEthanG@gmail.com>
2021-09-03 20:28:35 -0600

    EOD work, most views look pretty how they should

---
+++ Budgeter/Budgeter/View Controllers/Balance/BalanceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BalanceViewController.swift
+//  Budgeter
+//
+//  Created by Ethan Andersen on 9/3/21.
+//
+
+import UIKit
+
+class BalanceViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    } // End of View did load
+
+} // End of Class
